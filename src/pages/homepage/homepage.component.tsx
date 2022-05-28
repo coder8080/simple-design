277f74a6304af9fea97f713c3847ca2c1195f69c
Roman Kolerov <coder8080@mail.ru>
2022-05-28 22:35:32 +0300

    Adding layout section & fixed header

--- src/pages/homepage/homepage.component.tsx
+++ src/pages/homepage/homepage.component.tsx
@@ -2,8 +2,13 @@ import { FC } from 'react'
 import Clock from '../../assets/clock.svg'
 import Mark from '../../assets/mark.svg'
 import Wallet from '../../assets/wallet.svg'
-import Room1 from '../../assets/room1.png'
 import Room1Small from '../../assets/room1-small.png'
+import Pdf from '../../assets/pdf.svg'
+
+import Drawing1Background from '../../assets/drawing1-background.png'
+import Drawing1Main from '../../assets/drawing1-main.png'
+import Drawing1Rotated from '../../assets/drawing1-rotated.png'
+
 import Button from '../../components/button/button.component'
 import './homepage.styles.scss'
 
@@ -89,6 +94,57 @@ const Homepage: FC = () => (
       </div>
       <div className="image-container">
         <img src={Room1Small} className="small-img" alt="Room" />
+      </div>
+    </section>
+    <section className="section out-services">
+      <h1 className="section-title h1">Наши услуги</h1>
+      <div className="layout-container">
+        <div className="text-container">
+          <h2 className="h2 title">Планировка за 99 000 ₸</h2>
+          <span className="subtitle h3">
+            Срок выполнения: <span className="time">5 дней</span>
+          </span>
+          <p className="text h3">
+            Мы разработаем для вас уникальное планировочное решение квартиры,
+            которое будет соответствовать как нормам эргономики, так и вашим
+            личным пожеланиям, а также подготовим чертеж для согласования
+            планировки.
+          </p>
+          <div className="points-container">
+            <ul className="points h3">
+              <li className="point">2 варианта планировки + финальный</li>
+              <li className="point">обмерный план</li>
+              <li className="point">план демонтажа стен</li>
+              <li className="point">план монтажа перегородок</li>
+            </ul>
+          </div>
+          <div className="application-container">
+            <Button>Оставить заявку</Button>
+            <span className="view-example">
+              <div className="icon-container">
+                <img src={Pdf} alt="PDF" />
+              </div>
+              <span className="application-text">Посмотреть пример</span>
+            </span>
+          </div>
+        </div>
+        <div className="drawing-container">
+          <img
+            src={Drawing1Background}
+            alt="Background"
+            className="backround-img"
+          />
+          <img
+            src={Drawing1Main}
+            alt="Drawing1-Main"
+            className="drawing-main"
+          />
+          <img
+            src={Drawing1Rotated}
+            alt="Drawing1-Second-Page"
+            className="drawing-rotated"
+          />
+        </div>
       </div>
     </section>
   </div>
