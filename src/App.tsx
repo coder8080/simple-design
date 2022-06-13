bc4b446d85f069db7796a43e6ea7d3f0c4105cd0
Roman Kolerov <coder8080@mail.ru>
2022-06-13 14:29:51 +0300

    Removing unused react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import Homepage from './pages/homepage/homepage.component'
-import AboutUs from './pages/about-us/about-us.component'
 import Header from './components/header/header.component'
 import Footer from './components/footer/footer.component'
-import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-      </Routes>
+      <Homepage />
       <Footer />
     </div>
   )
--- src/pages/about-us/about-us.component.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react'
-import './about-us.styles.scss'
-
-const AboutUs: FC = () => <div className="about-us page">About Us Page</div>
-
-export default AboutUs
